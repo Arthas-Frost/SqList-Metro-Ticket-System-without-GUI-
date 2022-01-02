6637062a5ff3ee965946b9752448e20f8aa98d34
Arthas-Frost <72306936+Arthas-Frost@users.noreply.github.com>
2022-01-02 12:34:37 +0800

    Add files via upload

---
+++ Metro.cpp
@@ -0,0 +1,138 @@
+﻿// Metro.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include "Sqlist.h"
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+SqList L;
+metro num1[200];
+int show_identity(bool a)//身份输出函数
+{   
+
+    switch (a)
+    {
+        case 0:
+            cout << "您的身份为:乘客\n"<<endl;
+            break;
+        case 1:
+            cout << "您的身份为:管理员\n" << endl;
+            break;
+    }
+    return 0;
+}
+void window(const char name[10])//窗口管理函数
+{
+    if (name == "head") {//封装
+        cout << "-------------------------------------------------------------------\n欢迎使用OGAS地铁订票系统\n请选择您要执行的操作:\n" << endl;
+    }
+    if (name == "boot") {//封装
+        cout << show_identity(identity)+"-------------------------------------------------------------------\n" << endl;
+    }
+    if (name == "main") {
+        switch (identity) {
+
+        case 0:
+            window("head");
+            cout << "\t切换身份：/id\n" << endl;
+            cout << "\t查询车站信息：/find\n" << endl;
+            cout << "\t查询票价：/buy\n" << endl;
+            cout << "\t票价列表：/cost\n" << endl;
+            cout << "\t列出所有站：/list\n" << endl;
+            window("boot");
+            break;
+
+        case 1:
+            window("head");
+            cout << "\t切换身份：/id\n" << endl;
+            cout << "\t查询车站信息：/find\n" << endl;
+            cout << "\t查询票价：/buy\n" << endl;
+            cout << "\t票价列表：/cost\n" << endl;
+            cout << "\t列出所有站：/list\n" << endl;
+            cout << "\t编辑车站：/edit\n" << endl;
+            cout << "\t新增车站：/adds\n" << endl;
+            cout << "\t删除车站：/remove\n" << endl;
+            cout << "\t按客流量排序：/sort\n" << endl;
+            window("boot");
+            break;
+
+        }
+    }
+}
+
+void id_change()
+{
+    string password ="";
+    cout << "请输入密码：\n" << endl;
+    cin >> password;
+    if (password == "OGAS") {
+        identity = !identity;
+        window("main");
+    }
+    else {
+        cout << "密码错误，返回主页面\n" << endl;
+        window("main");
+    }
+       
+}
+
+void sortselect() {
+    int choice = 0;
+    cout << "请选择您要使用的排序方式" << endl;
+    cout << "1 折半插入排序\n" <<"2 冒泡排序\n"<<"3 希尔排序（不稳定）\n" <<"4 二路归并排序\n" << "5 快速排序（不稳定）\n" << endl;
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            BISort(L, L.length);
+            break;
+        case 2:
+            bubble(L);
+        case 3:
+            ShellSort(L, L.length);
+            break;
+        case 4:
+            Mergesort(L, L.length);
+            break;
+        case 5:
+            FSprint(L);
+        default: 
+        cout << "请输入有效的排序序号" << endl;
+        break;
+    }
+}
+void command() //命令控制函数
+{
+        string command;
+        cout<<"#"; cin >> command;
+        if (command == "/id")id_change();
+        else if (command == "/price")Price(&L);
+        else if (command == "/list")Printall(&L);
+        else if (command == "/cost")Cost(&L);
+        else if (command == "/edit" && identity == 1)edit(&L);
+        else if (command == "/adds" && identity == 1)adds(&L);
+        else if (command == "/remove" && identity == 1)remove(&L);
+        else if (command == "/sort" && identity == 1)sortselect();
+        else cout << "请输入正确的指令\n" << endl;
+}
+
+int main()
+{
+    srand(time(NULL));
+    CreateList(&L,num1);
+    window("main");
+    while (1) { command(); };
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ Sqlist.h
@@ -0,0 +1,433 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+#include <malloc.h>
+#include <math.h>
+using namespace std;
+bool identity = 0;//0为乘客，1为管理员
+typedef struct {
+	string name;
+	int flow;
+	int mid;
+}metro;
+
+typedef struct {
+	metro detail[200];
+	int length;
+}SqList;
+
+void CreateList(SqList* L, metro detail[]) {
+	L->detail[0].name = "郑州火车站";
+	L->detail[0].flow = rand()/20;
+	L->detail[0].mid = 1;
+
+	L->detail[1].name = "二七广场";
+	L->detail[1].flow = rand()/20;
+	L->detail[1].mid = 2;
+
+	L->detail[2].name = "人民路";
+	L->detail[2].flow = rand()/20;
+	L->detail[2].mid = 3;
+
+	L->detail[3].name = "紫荆山";
+	L->detail[3].flow = rand()/20;
+	L->detail[3].mid = 4;
+
+	L->detail[4].name = "燕庄";
+	L->detail[4].flow = rand()/20;
+	L->detail[4].mid = 5;
+	//L->length = sizeof(&L);
+	L->length = 5;
+}
+int Print(SqList* L,int i) {
+	cout << "地铁站ID:" << L->detail[i - 1].mid << endl;
+	cout << "地铁站名:" << L->detail[i - 1].name << endl;
+	switch (identity) {
+	case 1:
+		cout << "地铁站客流量:" << L->detail[i - 1].flow << endl;
+	}
+	cout << "\n" << endl;
+	return 0;
+}
+
+int Printall(SqList* L) {
+	for (int i = 1; i <= L->length; i++) {
+		Print(L, i);
+	}
+	return 0;
+}
+int Printlist(SqList* L,unsigned i, unsigned o ) {
+	if (i >= o){
+		for (i; i>=o+1;i--) {
+			cout << L->detail[i - 1].name << "-->";
+		}
+		cout << L->detail[i - 1].name<<endl;
+	}
+	else if (i <= o-1) {
+		for (i; i <= o-1; i++) {
+			cout << L->detail[i - 1].name << "-->";
+		}
+		cout << L->detail[i - 1].name << endl;
+	}
+	return 0;
+}
+
+int Find(SqList* L, string station)
+{
+	int i = 1;
+	bool find = 0;
+	for (i = 1; i <= L->length; i++) {
+		if (L->detail[i - 1].name == station) {
+			find = 1;
+			break;
+		}
+	}
+	switch (find) {
+	case 0:
+		cout << "未找到" << endl;
+		return 0;
+		break;
+	case 1:
+		Print(L, i);
+		return i;
+		break;
+	}
+
+}
+
+int Price(SqList* L) {
+	int in = 0;
+	int out = 0;
+	int distance = 0;
+	string Getin = "0";
+	string Getoff = "0";
+	cout << "请输入上车站站名" << endl;
+	cin >> Getin;
+	in = Find(L, Getin);
+	cout << "请输入下车站站名" << endl;
+	cin >> Getoff;
+	out = Find(L, Getoff);
+	switch (in && out) {
+		case 0:
+			cout << "输入有误，返回上层页面" << endl;
+			break;
+		default:
+			distance = abs(out - in);
+			if (distance <= 2) {
+				cout << "票价2元" << endl;
+				cout << "路线：\n" << endl;
+				Printlist(L, in,out);
+			}
+			if (distance > 2)
+			{
+				cout << "票价" << (distance+1)/2+1 << "元"<<endl;
+				cout << "路线：\n" << endl;
+				Printlist(L, in, out);
+			}
+	}
+	return 0;
+}
+
+int Cost(SqList* L) {
+	int in = 0;
+	int out = 0;
+	int distance = 0;
+	string Getin = "0";
+	cout << "请输入上车站站名" << endl;
+	cin >> Getin;
+	in = Find(L, Getin);
+	for (out = in+1; out <=L->length; out++) {
+		switch (in && out) {
+		case 0:
+			cout << "输入有误，返回上层页面" << endl;
+			break;
+		default:
+			distance = abs(out - in);
+			if (distance <= 2) {
+				cout << "路线：" ;
+				Printlist(L, in, out);
+				cout << "票价2元\n" << endl;
+			}
+			if (distance > 2)
+			{
+				cout << "路线：" ;
+				Printlist(L, in, out);
+				cout << "票价" << (distance + 1) / 2 + 1 << "元\n" << endl;
+			}
+		}
+	}
+	for (out = in-1; out>=1; out--) {
+		switch (in && out) {
+		case 0:
+			cout << "输入有误，返回上层页面" << endl;
+			break;
+		default:
+			distance = abs(out - in);
+			if (distance <= 2) {
+				cout << "路线：" ;
+				Printlist(L, in, out);
+				cout << "票价2元\n" << endl;
+			}
+			if (distance > 2)
+			{
+				cout << "路线：" ;
+				Printlist(L, in, out);
+				cout << "票价" << (distance + 1) / 2 + 1 << "元\n" << endl;
+			}
+		}
+	}
+	return 0;
+}
+
+int edit(SqList* L) {
+	int sid = 0;
+	string oname="0";
+	string nname="0";
+	string choice="0";
+	int nflow = 0;
+	cout << "请输入您要修改的站名" << endl;
+	cin >> oname;
+	sid=Find(L, oname);
+	if (sid > 0) {
+		cout << "该站点信息如下" << endl;
+		Print(L, sid);
+		cout << "请输入新的站名" << endl;
+		cin >> nname;
+		cout << "请输入新的客流量" << endl;
+		cin >> nflow;
+		cout << "站名：" << L->detail[sid - 1].name << "-->" << nname << endl;
+		cout << "客流：" << L->detail[sid - 1].flow << "-->" << nflow << endl;
+		cout << "确认要更改吗？y/n" << endl;
+		cin >> choice;
+		if (choice == "y") {
+			L->detail[sid - 1].name = nname;
+			L->detail[sid - 1].flow = nflow;
+			cout << "修改已保存"<<endl;
+		}
+		if (choice == "n") {
+			cout << "修改未保存" << endl;
+		}
+		}
+	return 0;
+	}
+
+int adds(SqList* L){
+	string name="0";
+	string choice = "0";
+	int sid=0;
+	int flow = 0;
+	cout << "请输入您要新增的站点的站点名" << endl;
+	cin >> name;
+	cout << "请输入您要新增的站点ID" << endl;
+	cin >> sid;
+	cout << "请输入您要新增的站点的客流量" << endl;
+	cin >> flow;
+	cout << "您要新增的站点信息如下" << endl;
+	cout << "站点名：" << name << "\n站点ID：" << sid << "\n站点客流量："<< flow << endl;
+	cout << "您确定要新增这个站点吗？y/n" << endl;
+	cin >> choice;
+	if (choice == "y") {
+		for (int i = L->length; i >= sid; i--) {
+			L->detail[i] = L->detail[i - 1];
+		}
+		L->detail[sid - 1].name = name;
+		L->detail[sid - 1].flow = flow;
+		L->detail[sid - 1].mid = sid;
+		L->length = L->length + 1;
+		cout << "已增加站点" << endl;
+	}
+	else if (choice == "n") {
+		cout << "您选择不增加这个站点" << endl;
+	}
+
+	return 0;
+}
+
+int remove(SqList* L) {
+	string name="0";
+	string choice = "0";
+	int sid = 0;
+	cout << "请输入您要删除的站点名" << endl;
+	cin >> name;
+	sid=Find(L, name);
+	cout << "您是否要删除这个站点？y/n" << endl;
+	cin >> choice;
+	if (choice == "y") {
+		for (int i = sid; i < L->length; i++) {
+			L->detail[i-1] = L->detail[i];
+		}
+		L->length = L->length - 1;
+		cout << "已删除站点" << endl;
+	}
+	else if (choice == "n") {
+		cout << "您选择不删除这个站点" << endl;
+	}
+
+	return 0;
+}
+
+int ShellSort(SqList Li, int n) {
+	int i, j, d;
+	metro tmp;
+	d = n / 2;
+	while (d > 0)
+	{
+		for (i = d; i < n; i++)
+		{
+			tmp = Li.detail[i];
+			j = i - d;
+			while (j >= 0 && tmp.flow < Li.detail[j].flow)
+			{
+				Li.detail[j + d] = Li.detail[j];
+				j = j - d;
+			}
+			Li.detail[j + d] = tmp;
+		}
+		d = d / 2;
+	}
+	Printall(&Li);
+	return 0;
+}
+
+int partition(SqList* Li,int s,int t) {
+	int i = s, j = t;
+	metro tmp = Li->detail[i];
+	while (i < j)
+	{
+		while (j > i && Li->detail[i].flow >= tmp.flow)
+		{
+			j--;
+		}
+		Li->detail[i] = Li->detail[j];
+		while (i < j && Li->detail[i].flow <= tmp.flow)
+		{
+			i++;
+		}
+		Li->detail[j] = Li->detail[i];
+	}
+	Li->detail[i] = tmp;
+	//cout << "######我是第" << i << "趟划分" << endl;
+	return i;
+}
+
+int FastSort(SqList* Li,int s,int t) {
+	int i;
+	if (s < t)
+	{
+		i = partition(Li, s, t);
+		FastSort(Li,s,i-1);
+		FastSort(Li,i+1,t);
+	}
+	//cout << "完成" << endl;
+	return 0;
+}
+
+int FSprint(SqList L){
+	FastSort(&L, 1, L.length);
+	Printall(&L);
+	return 0;
+}
+
+
+ void Merge(SqList* Li,int low,int mid,int high) {
+	SqList* Li1=NULL;
+	SqList Lt = *Li;
+	Li1 = &Lt;
+	int i = low, j = mid + 1, k = 0;
+	Li1 = (SqList*)malloc((high - low + 1) * sizeof(SqList));
+	while (i <= mid && j <= high) {
+		if (Li->detail[i].flow <= Li->detail[j].flow)
+		{
+			Li1->detail[k] = Li->detail[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			Li1->detail[k] = Li->detail[j];
+			j++;
+			k++;
+		}
+	}
+	while (i <= mid) {
+		Li1->detail[k] = Li->detail[i];
+		i++; 
+		k++;
+	}
+	while (j <= high) {
+		Li1->detail[k] = Li->detail[j];
+		j++;
+		k++;
+	}
+	for (k = 0, i = low; i <= high; k++, j++)
+	{
+		Li->detail[i] = Li1->detail[k];
+	}
+	free(Li1);
+}
+void MergePass(SqList* Li, int length, int n)
+{
+	int i = 0;
+	for (i = 0; i + 2 * length - 1 < n; i = i + 2 * length)
+		Merge(Li, i, i + length - 1, i + 2 * length - 1);
+	if (i + length - 1 < n - 1)
+		Merge(Li, i, i + length - 1, n - 1);
+}
+void Mergesort(SqList Li,int n) {
+	int length=0;
+	for (length = 1; length < n; length = 2 * length)
+		MergePass(&Li, length, n);
+	Printall(&Li);
+}
+
+void bubble(SqList L) {
+	bool flag = 1;
+	metro swap;
+	for (int i = 0; i < L.length-1; i++) {
+		for (int j = L.length-1; j > i; j--) {
+			flag = 0;
+			if (L.detail[j].flow < L.detail[j - 1].flow) {
+				swap = L.detail[j];
+				L.detail[j] = L.detail[j - 1];
+				L.detail[j - 1] = swap;
+				flag = 1;
+			}
+		}
+		if (flag == 0) {
+			break;
+		}
+	}
+	Printall(&L);
+}
+
+void BISort(SqList L,int n) {
+	int i = 0; 
+	int j = 0; 
+	int low = 0;
+	int high = 0;
+	int mid = 0;
+	metro tmp;
+	for (i = 1; i < n; i++) {
+		if (L.detail[i].flow < L.detail[i - 1].flow) {
+			tmp = L.detail[i];
+			low = 0;
+			high = i - 1;
+			while (low <= high) {
+				mid = (low + high) / 2;
+				if (tmp.flow < L.detail[mid].flow) {
+					high = mid - 1;
+				}
+				else {
+					low = mid + 1;
+				}
+			}
+			for (j = i - 1; j >= high + 1; j--) {
+				L.detail[j + 1] = L.detail[j];
+			}
+			L.detail[high + 1] = tmp;
+		}
+		
+	}
+	Printall(&L);
+}
